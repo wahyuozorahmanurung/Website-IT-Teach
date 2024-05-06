e567afb91bec40142bd7278b8b5aafe55a721272
WAHYU OZORAH MANURUNG <125001680+wahyuozorahmanurung@users.noreply.github.com>
2024-05-06 19:40:50 +0700

    Add files via upload

---
+++ kelompok4fix/Script/order.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    var tbl = document.getElementById("myTable");
+    var row = tbl.insertRow();
+    var cell1 = row.insertCell();
+    var cell2 = row.insertCell();
+    var cell3 = row.insertCell();
+    var cell4 = row.insertCell();
+    var cell5 = row.insertCell();
+    cell1.innerHTML = "S321";
+    cell2.innerHTML = "S321";
+    cell3.innerHTML = "S321";
+    cell4.innerHTML = "S321";
+    cell5.innerHTML = "S321";
+}
+
+
